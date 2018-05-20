babd397361559758133ea3326a8dae601f898c8e
boukhemis11 <boukhemis2011@gmail.com>
2018-05-20 18:35:08 +0200

    add formular

--- Node express mongodb/app.js
+++ Node express mongodb/app.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const app = express();
+const upload = multer();
 
 const PORT = 3000;
 
+let frenchMovies = [];
+
 app.use('/public', express.static('public' ));
+app.use(bodyParser.urlencoded({ extended:false}));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/movies', (req, res) => {
-//res.send('soon');
-res.render('movies');
+    const title = 'french movies';
 
+  frenchMovies = [
+      {title: 'helle1', year: 2016},
+      {title: 'helle2', year: 2017},
+      {title: 'helle3', year: 2018}
+    ];
+  res.render('movies', {movies: frenchMovies, title:title});
+
+});
+
+app.post('/movies', (req, res) => {
+  
+  let newFilm = {title: req.body.movietitle, year: req.body.movieyear};
+  frenchMovies.push(newFilm);
+  res.sendStatus(201);
+  console.log(frenchMovies);
 });
 
 app.get('/movies/add', (req, res) => {
@@ -21,14 +41,12 @@ app.get('/movies/add', (req, res) => {
 app.get('/movies/:id/:title', (req, res) => {
     const id = req.params.id;
     const title = req.params.title;
-    //res.send(`film ${id}`);
     res.render('movie-details', {movieid: id, movietitle: title});
 
 });
 
 
 app.get('/', (req, res) => {
-//res.send('Hello Boukh');
 res.render('index');
 });
 
