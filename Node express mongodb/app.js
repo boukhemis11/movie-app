c31a60bc8a7054aa960da0234a07af61889abd07
boukhemis11 <boukhemis2011@gmail.com>
2018-05-20 12:49:46 +0200

    add template ejs

--- Node express mongodb/app.js
+++ Node express mongodb/app.js
@@ -3,17 +3,29 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/movie', (req, res) => {
+app.use('/public', express.static('public' ));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/movies', (req, res) => {
 res.send('soon');
+
 });
 
-app.get('/movie/:id', (req,res) => {
+app.get('/movies/add', (req, res) => {
+    res.send('page add filme');
+});
+
+app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
     res.send(`film ${id}`);
-})
+});
+
 
 app.get('/', (req, res) => {
-res.send('Hello Boukh');
+//res.send('Hello Boukh');
+res.render('index');
 });
 
 app.listen(3000, () => {
