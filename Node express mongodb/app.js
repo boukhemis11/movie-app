d410f3410f8417cebde3e0d2b8350d0d4b878286
boukhemis11 <boukhemis2011@gmail.com>
2018-05-20 12:12:59 +0200

    add root movie

--- Node express mongodb/app.js
+++ Node express mongodb/app.js
@@ -3,8 +3,17 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/movie', (req, res) => {
+res.send('soon');
+});
+
+app.get('/movie/:id', (req,res) => {
+    const id = req.params.id;
+    res.send(`film ${id}`);
+})
+
 app.get('/', (req, res) => {
-res.send('Hello Boukh')
+res.send('Hello Boukh');
 });
 
 app.listen(3000, () => {
